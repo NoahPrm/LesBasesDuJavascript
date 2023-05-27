34390415e082375b570700c83f990dfdc3d49e5d
Noah <108767214+Mini-Noah@users.noreply.github.com>
2023-05-27 15:00:22 +0200

    Update maths.js

--- maths.js
+++ maths.js
@@ -1,1 +1,11 @@
+// Arrondi un nombre 
+Math.floor() 
 
+// Arrondi un nombre
+Math.round()
+
+// Genère un nombre (décimal/entier) entre 85
+Math.random() * 85
+
+// Genère un nombre (entier) entre 0 et 85
+Math.floor(Math.random() *85)
