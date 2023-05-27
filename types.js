7d1da59edc4cc3e8d04f42756c5ce010118d89db
Noah <108767214+Mini-Noah@users.noreply.github.com>
2023-05-27 14:19:22 +0200

    Update types.js

--- types.js
+++ types.js
@@ -1,1 +1,11 @@
-
+object = {}
+string = ""
+numbers = 123
+array = []
+bigint = 132a
+/*function*/ = f = function() {
+    console.log("test").
+}
+boolean = true/false
+null = null
+undefined = undefined
