8d6504bc41d5bb54a528ca541bd4671ad99d3db1
Noah <108767214+Mini-Noah@users.noreply.github.com>
2023-05-27 14:44:35 +0200

    Update syntaxes.js

--- syntaxes.js
+++ syntaxes.js
@@ -1,1 +1,64 @@
+// Est égal
+if(test === "test") {
+  testtqd
+  qzdqdsdz
+  qdqsesg
+}
 
+// N'est pas égale :
+if(test !== "test") {
+  qsdz
+  qdqdq
+  gves
+}
+
+// Existe pas :
+if(!test) {
+  qsdzq
+  qfzq
+  qfsgs
+}
+
+// Si (...) sinon (...) :
+if(test === "test") {
+  qzdqs
+  fqz
+}
+else fesdfsgg
+
+// Arreter le code si test n'est pas égal à test :
+if(test !== "test") return;
+
+// Arreter le code si test n'est pas égal à test + autres choses à faire :
+if(test !== "test") {
+  console.log("qdz")
+  sefs
+  return;
+};
+
+// Boucle qui répète 4 fois un tableau pour monter ce qui est écrit dedans :
+let array = ["tf", 12, 34, {}]
+for(let i = 0; 1 < array.length; i++ {
+  array[i]
+}
+
+// Boucle qui donnera un par un les personnes bannis en boucle jusqu'à qu'il n'y en ai plus :
+let bans = new Map()
+    for(const ban of bans) {
+  ban
+}
+
+// Effectue le code après le try mais s'il y a une erreur, fait ce qu'il y a apres le catch :
+try {
+  dzqsd
+  qzdedz
+  rgdrg
+} catch (err) {
+  
+  sefsfsf
+}
+    
+// Exporte le code :
+module.exports = {
+  zdqdqz
+}
